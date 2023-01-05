f1c9dd6beeb32dc44a31ccd84ef8852fa833887a
Ryad Amir <ryadamir@hotmail.com>
2023-01-05 14:17:08 +0100

    code optimization

--- app/src/main/java/com/ryadamir/movieapp/AppController.kt
+++ app/src/main/java/com/ryadamir/movieapp/AppController.kt
@@ -3,8 +3,6 @@ package com.ryadamir.movieapp
 import android.app.Application
 import com.ryadamir.movieapp.di.dataModule
 import com.ryadamir.movieapp.di.viewModelModule
-import io.realm.Realm
-import io.realm.RealmConfiguration
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -18,17 +16,5 @@ class AppController : Application() {
             modules(dataModule)
             modules(viewModelModule)
         }
-
-        Realm.init(this)
-
-        val config = RealmConfiguration.Builder()
-            .allowWritesOnUiThread(true)
-            .allowQueriesOnUiThread(true)
-            .build()
-
-        Realm.setDefaultConfiguration(config)
-
     }
-
-
 }
